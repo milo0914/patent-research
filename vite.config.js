3caf0d065ae2b273409b525ba37c11dc18786d8e
milo0914 <hemohuang@gmail.com>
2025-08-17 21:45:41 +0800

    Feat: Update config files to v2

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/patent-research/',
-  define: {
-    'React': 'React',
-  },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      external: ['React'],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
